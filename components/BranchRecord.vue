77669abccf7d4ce30419689a27b3b2daff43b150
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-09-18 02:37:55 +0530

    Create BranchRecord.vue

---
+++ components/BranchRecord.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex justify-between text-lg p-2 text-gray-900 transition">
+        <span class="w-[5%] selection:text-lblue font-fira text-base my-auto">{{ item.branchID }}</span>
+        <span class="truncate font-sans w-[15%] font-thin hover:cursor-pointer selection:text-lblue">{{ item.name }}</span>
+        <span class="truncate font-sans w-[40%] font-thin hover:cursor-pointer selection:text-lblue">{{ item.address }}</span>
+        <span class="text-base font-thin my-auto">{{ String(index + 1).padStart(5, '0') }}</span>
+    </div>
+</template>
+
+<script setup>
+    const { item, index } = defineProps(['item', 'index'])
+</script>
