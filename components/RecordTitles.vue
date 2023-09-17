5d7a4cf567b4eb01382e0495de42ffa90cd13dcd
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-09-18 02:37:09 +0530

    re-stagings

--- components/RecordTitles.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="flex justify-between text-xl mb-4 px-2 font-thin text-lblue my-2">
-        <span>Identifier</span>
-        <span>Destination</span>
-        <span>@</span>
-    </div>
-</template>
