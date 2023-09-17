605c4f6c95e1fcc2dd5db8e49d48b851ab882b86
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-09-18 02:37:50 +0530

    Update index.vue

--- pages/branches/index.vue
+++ pages/branches/index.vue
@@ -3,15 +3,17 @@
         <div class="container flex flex-col">
             <div class="flex gap-4">
                 <input type="text" placeholder="ID" class="sm:w-[35%] w-[100%] text-xl font-thin p-3 my-2 border focus:border-lblue rounded-lg outline-none transition">
-                <input type="text" placeholder="Domain" class="sm:w-[35%] w-[100%] text-xl font-thin p-3 my-2 border focus:border-lblue rounded-lg outline-none transition">
+                <input type="text" placeholder="Name" class="sm:w-[35%] w-[100%] text-xl font-thin p-3 my-2 border focus:border-lblue rounded-lg outline-none transition">
             </div>
             <button class="w-fit p-3 bg-lblue rounded-lg text-white text-lg">Lookup</button>
         </div>
         <div class="container">
-            <RecordTitles/>
+            <div class="flex justify-between text-xl mb-4 px-2 font-thin text-lblue my-2">
+                <span>@ ID</span>
+            </div>
             <span v-if="pending" class="flex justify-center text-lg px-2">Loading...</span>
-            <div v-else v-for="(item, index) in directory.data">
-                <Record :item="item" :index="index"/>
+            <div v-else v-for="(item, index) in branches.data">
+                <BranchRecord :item="item" :index="index"/>
             </div>
         </div>
     </div>
@@ -22,7 +24,11 @@
         title: 'Admin | Branches'
     })
 
-    const { public: { API } } = useRuntimeConfig()
+    const { public: { API, TOKEN } } = useRuntimeConfig()
 
-    const { data: directory, pending } = await useLazyFetch(API + '/api/directory')
+    const { data: branches, pending } = await useLazyFetch(API + '/api/panel/branches', {
+        headers: {
+            authorization: `Bearer ${ TOKEN }`
+        }
+    })
 </script>
